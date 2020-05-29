714bef692f0461bc8fbbb1a200155eb5de14288b
Kevin Hu <khu@liveperson.com>
2020-05-29 13:06:58 -0400

    added fe logs

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -88,10 +88,11 @@ export class LoginComponent implements OnInit {
 
   onSubmit() {
     window.open('/api/auth/login',"mywindow","location=1,status=1,scrollbars=1, width=800,height=800");
-    window.addEventListener('message', (message) => {
+    let listener = window.addEventListener('message', (message) => {
       //message will contain facebook user and details
-      console.log(message)
+      console.log(message.data.user)
       localStorage.setItem("bearer", message.data.user);
+      console.log(window.localStorage.bearer)
       this.router.navigate(["/dashboard"])
     });    
   }
