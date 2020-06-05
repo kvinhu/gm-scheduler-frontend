c9b4617359d56d49e1c802ccedbfa5c6b5c6c4e9
Kevin Hu <khu@liveperson.com>
2020-06-05 16:37:18 -0400

    edit dashboard init

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 import { LineOfService } from "../shared/interfaces";
 import { GetServicesService } from "../services/get-services.service";
-import { timesTo_hhmmss } from '../shared/helpers';
-import { LoginService } from '../services/login.service';
-import { Router } from '@angular/router';
+import { timesTo_hhmmss } from "../shared/helpers";
+import { LoginService } from "../services/login.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-dashboard",
@@ -15,29 +15,40 @@ export class DashboardComponent implements OnInit {
   view: LineOfService;
   loggedIn: boolean = false;
 
-  constructor(private los: GetServicesService, private login: LoginService, private router: Router) {
-    
-  }
+  constructor(
+    private los: GetServicesService,
+    private login: LoginService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
-    let bearer = localStorage.getItem('bearer');
-    if(!bearer) {
-          this.router.navigate(['login'])
-    } else {
-      this.login.getToken(bearer).subscribe((message: any) => {
-        console.log(message)
-        this.loggedIn = true;
-        this.los.getAll().subscribe((services: LineOfService[]) => {
-          this.services = services.map(service => timesTo_hhmmss(service));
-          this.view = services[0];
-        });
-      },
-      (err) => {
-        console.log(err)
-        //localStorage.setItem("bearer", "");
-          //this.router.navigate(['login'])
-      })
-    }
+    this.localStorageGetItem("bearer").then(bearer => {
+      if (!bearer) {
+        this.router.navigate(["login"]);
+      } else {
+        this.login.getToken(bearer).subscribe(
+          (message: any) => {
+            console.log(message);
+            this.loggedIn = true;
+            this.los.getAll().subscribe((services: LineOfService[]) => {
+              this.services = services.map(service => timesTo_hhmmss(service));
+              this.view = services[0];
+            });
+          },
+          err => {
+            console.log(err);
+            //localStorage.setItem("bearer", "");
+            //this.router.navigate(['login'])
+          }
+        );
+      }
+    });
+  }
+
+  localStorageGetItem(key: string) {
+    return Promise.resolve().then(() => {
+      return localStorage.getItem(key);
+    });
   }
 
   public onChange(event): void {
